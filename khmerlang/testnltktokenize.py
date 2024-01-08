2aabea11c4d0a0c12f4839c962bc77d3766042b2
Mark <mkstreet.bangkok@gmail.com>
2024-01-08 10:43:10 +0700

    Initial Setup

---
+++ khmerlang/testnltktokenize.py
@@ -0,0 +1,13 @@
+from nltk.tokenize import word_tokenize
+
+
+
+tokens = word_tokenize("Hello, the quick brown fox.")
+
+print(tokens)
+
+
+
+tokens = word_tokenize("ស្រមោច  ស្រមោចក្រហម  ស្រមោចខ្មៅ  ពាំចំណី  ទៅទីជំរក  ក្ងាន  មានកវែង  រោមពណ៌ស  រស់នៅ  ជាមួយគ្នា ស៊ីអាហារ ឆ្មាពាំចំណីទៅអោយកូន ។")
+
+print(tokens)

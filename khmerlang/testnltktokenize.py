4302ba314e3c64f44782f33676362bbadfeda6d2
Mark <mkstreet.bangkok@gmail.com>
2024-01-08 19:27:50 +0700

    update environment

--- khmerlang/testnltktokenize.py
+++ khmerlang/testnltktokenize.py
@@ -9,3 +9,11 @@ from khmernltk import word_tokenize
 
 tokens = word_tokenize("ក្រោយចេញពីសាលា ខ្ញុំទៅហាត់ប្រដាល់ថៃ។ ខ្ញុំរីករាយនឹងកីឡានេះណាស់។")
 print(tokens)
+
+
+
+file = open('output.txt', 'w', encoding='utf8') 
+for tok in tokens:
+    file.write(tok + "\n")
+
+file.close()

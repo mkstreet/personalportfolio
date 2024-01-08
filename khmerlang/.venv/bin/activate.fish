687d6332d6e1b2c6a5bed3e454bebd67c4ecd05b
Mark <mkstreet.bangkok@gmail.com>
2024-01-08 19:21:59 +0700

    update environment

--- khmerlang/.venv/bin/activate.fish
+++ khmerlang/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mark/gitprojects/personalportfolio/khmerlang/.venv"
+set -gx VIRTUAL_ENV "/home/mark-bmax2/gitprojects/personalportfolio/khmerlang/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- khmerlang/testnltktokenize.py
+++ khmerlang/testnltktokenize.py
@@ -1,13 +1,11 @@
-from nltk.tokenize import word_tokenize
+#from nltk.tokenize import word_tokenize
+from khmernltk import word_tokenize
+
+
+# tokens = word_tokenize("Hello, the quick brown fox.")
+# print(tokens)
 
 
 
-tokens = word_tokenize("Hello, the quick brown fox.")
-
-print(tokens)
-
-
-
-tokens = word_tokenize("ស្រមោច  ស្រមោចក្រហម  ស្រមោចខ្មៅ  ពាំចំណី  ទៅទីជំរក  ក្ងាន  មានកវែង  រោមពណ៌ស  រស់នៅ  ជាមួយគ្នា ស៊ីអាហារ ឆ្មាពាំចំណីទៅអោយកូន ។")
-
+tokens = word_tokenize("ក្រោយចេញពីសាលា ខ្ញុំទៅហាត់ប្រដាល់ថៃ។ ខ្ញុំរីករាយនឹងកីឡានេះណាស់។")
 print(tokens)

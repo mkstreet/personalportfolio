4323589ab1917a42b50232e286716c63f4b0379b
Mark <mkstreet.bangkok@gmail.com>
2024-01-10 15:09:11 +0700

    updates

--- khmerlang/main.py
+++ khmerlang/main.py
@@ -4,13 +4,24 @@ from lineoftext import LineOfText
 from wordfrequencytable import WordFrequencyTable
 from stats import Stats
 
-lot = LineOfText("ក្រោយចេញពីសាលា ខ្ញុំទៅហាត់ប្រដាល់ថៃ។ ខ្ញុំរីករាយនឹងកីឡានេះណាស់។")
+#lot = LineOfText("ក្រោយចេញពីសាលា ខ្ញុំទៅហាត់ប្រដាល់ថៃ។ ខ្ញុំរីករាយនឹងកីឡានេះណាស់។")
 
 st = Stats()
 wft = WordFrequencyTable()
 
-st.accumulateCounts(lot)
-wft.accumulateFrequency(lot)
+
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        lot = LineOfText(line)
+        st.accumulateCounts(lot)
+        wft.accumulateFrequency(lot)
+      
+
+
+#st.accumulateCounts(lot)
+#wft.accumulateFrequency(lot)
 
 st.printStats()
 wft.writeWordFreqList()
